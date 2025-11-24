0a7efe937f4fff8a682c2092041e316b163b99ce
Rafael Luis <rafaeluisx@gmail.com>
2025-11-23 22:52:19 -0300

    feat: adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Larissa, eu te amo, se você me ama, da uma risadinha!😎</h1>;
+function Home() {
+  return <h1>Larissa, eu te amo, se você me ama, da uma risadinha!😎</h1>;
 }
 
 export default Home;
