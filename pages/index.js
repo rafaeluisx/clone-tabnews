a787a3d35959570cfb57dca4725e1bfd4ccc3838
Rafael Luis <rafaeluisx@gmail.com>
2025-11-26 23:05:47 -0300

    feat: inclui mensagem de `em construção` temporariamente na página principal do projeto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Larissa, eu te amo, se você me ama, da uma risadinha!😎</h1>;
+  return (
+    <>
+      <h1>Página em construção 👷🏻🪚🔧</h1>
+      <h3>I see you're very anxious, huh? 🧐</h3>
+    </>
+  );
 }
 
 export default Home;
