27fd3a488d284fa6ecec787ceed6b65cc594aaca
Rafael Luis <rafaeluisx@gmail.com>
2026-09-06 20:49:16 -0300

    fix: `Eslint` linting

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <h1>Página em construção 👷🏻🪚🔧</h1>
-      <h3>I see you're very anxious, huh? 🧐</h3>
+      <h3>I see you&apos;re very anxious, huh? 🧐</h3>
     </>
   );
 }
